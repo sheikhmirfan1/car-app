76456f6027b10fa425d31610a0c91b426db1923b
Sheikhmirfan1 <Sheikhmirfan@yahoo.com>
2024-06-11 11:27:19 +0200

    update home

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import AppRouter from './AppRouter.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
+import App from './components/Home.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AppRouter />
-  </BrowserRouter>,
-)
+    <App />
+  </BrowserRouter>
+);
